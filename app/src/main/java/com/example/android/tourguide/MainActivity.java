78c5db876489751908e6056f6ad9e73d5eafd163
SilentOne <SilentOne@onet.pl>
2018-04-19 21:58:17 +0200

    Adding TabLayout - solved.

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.tourguide;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
